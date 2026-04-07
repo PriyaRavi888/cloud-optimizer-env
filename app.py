c37301461b44bb9835f2169287297a57aa0b8ea4
Priyadarshini Ravikumar <pdrk888@gmail.com>
2026-04-07 08:41:14 +0530

    added reset API

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from env.environment import CloudEnv
+
+app = FastAPI()
+
+env = CloudEnv()
+
+@app.post("/reset")
+def reset():
+    state = env.reset()
+    return {"status": "reset successful", "state": state}
+
+
+@app.get("/")
+def root():
+    return {"message": "Cloud Optimizer Environment Running"}
