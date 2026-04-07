28ba017fd18a91c6b536e701603a805f54af43c8
Priyadarshini Ravikumar <pdrk888@gmail.com>
2026-04-07 08:57:27 +0530

    fix server issue

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
+from fastapi import FastAPI
 import sys
 import os
 
 sys.path.append(os.path.abspath("."))
-from fastapi import FastAPI
+
 from env.environment import CloudEnv
 
 app = FastAPI()
-
 env = CloudEnv()
 
 @app.post("/reset")
 def reset():
     state = env.reset()
-    return {"status": "reset successful", "state": state}
-
+    return {"status": "ok", "state": state}
 
 @app.get("/")
 def root():
-    return {"message": "Cloud Optimizer Environment Running"}
+    return {"message": "running"}
