282b8566ec3667dba86416a5e7cd94d546887eaa
Priyadarshini Ravikumar <pdrk888@gmail.com>
2026-04-07 09:36:36 +0530

    added reset endpoint

--- inference.py
+++ inference.py
@@ -13,6 +13,7 @@ threading.Thread(target=run_env).start()
 
 # Simple server for Hugging Face
 class Handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
@@ -23,6 +24,19 @@ class Handler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
 
+    def do_POST(self):
+        if self.path == "/reset":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+
+            # minimal valid response
+            response = b'{"status": "reset successful"}'
+            self.wfile.write(response)
+
+        else:
+            self.send_response(404)
+            self.end_headers()
 server = HTTPServer(("0.0.0.0", 7860), Handler)
 print("Server running on port 7860...")
 server.serve_forever()
